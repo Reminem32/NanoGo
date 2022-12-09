6ba1340072303f7394f06db7b75341ee95312838
Toto <remi.baron06@gmail.com>
2022-12-09 18:05:13 +0100

    Lentement mais sûrement, les changements'effctuent

--- typing.ml
+++ typing.ml
@@ -22,21 +22,30 @@ let rec type_type = function
   | PTident { id = "bool" } -> Tbool
   | PTident { id = "string" } -> Tstring
   | PTptr ty -> Tptr (type_type ty)
-  | PTident {id;loc} -> 
-    try 
-        Tstruct(Hashtbl.find structure_env id)
-    with
-      Not_found -> error loc "unknown struct "
-  |_ -> error dummy_loc "unknown struct "
+  | PTident { id; loc } -> (
+      try Tstruct (Hashtbl.find structure_env id) with
+      | Not_found -> error loc "unknown struct "
+      | _ -> error dummy_loc "unknown struct ")
 (* TODO type structure *)
 
 let rec eq_type ty1 ty2 =
   match (ty1, ty2) with
   | Tint, Tint | Tbool, Tbool | Tstring, Tstring -> true
   | Tstruct s1, Tstruct s2 -> s1 == s2
+  | t1, Tmany [ t2 ] | Tmany [ t1 ], t2 -> eq_type t1 t2
+  | Tptr Twild, Tptr _ | Tptr _, Tptr Twild -> true
   | Tptr ty1, Tptr ty2 -> eq_type ty1 ty2
   | _ -> false
-(* TODO autres types *)
+(* TODONE autres types *)
+
+let unfold_typList = function
+  | [ Tmany l ] | l ->
+      List.map
+        (function
+          | Tmany (_ :: _ :: _) ->
+              error dummy_loc "Trop d'arguments dans ta fonction"
+          | Tmany [ t ] | t -> t)
+        l
 
 let fmt_used = ref false
 let fmt_imported = ref false
@@ -86,10 +95,10 @@ let make d ty = { expr_desc = d; expr_typ = ty }
 let stmt d = make d tvoid
 
 let rec a_t_on_une_l_value e =
-  match (e.pexpr_desc) with
-  | PEident _-> true
-  | PEdot (e,x) -> (a_t_on_une_l_value e)
-  | PEunop (Ustar, e) ->  (e.pexpr_desc <> PEnil)
+  match e.pexpr_desc with
+  | PEident _ -> true
+  | PEdot (e, x) -> a_t_on_une_l_value e
+  | PEunop (Ustar, e) -> e.pexpr_desc <> PEnil
   | _ -> false
 
 let rec expr env e =
@@ -106,29 +115,36 @@ and expr_desc env loc = function
       | Cstring x -> (TEconstant c, Tstring, false))
   | PEbinop (op, e1, e2) -> (
       (* TODONE *)
-      let toto1 = fst(expr env e1) in
-      let toto2 = fst(expr env e2) in
+      let toto1 = fst (expr env e1) in
+      let toto2 = fst (expr env e2) in
       match op with
       | Beq | Bne ->
-          if toto1.expr_desc = TEnil && toto2.expr_desc = TEnil then error loc "e1 ou e2 était nill"
-          else (TEbinop (op,toto1, toto2), Tbool, false)
+          if toto1.expr_desc = TEnil && toto2.expr_desc = TEnil then
+            error loc "e1 ou e2 était nill"
+          else (TEbinop (op, toto1, toto2), Tbool, false)
       | Badd | Bsub | Bmul | Bdiv | Bmod ->
           let a1, b1 = expr env e1 in
           let a2, b2 = expr env e2 in
-          if a1.expr_typ = Tint && a1.expr_typ = Tint then (TEbinop (op, a1, a2), Tint, false)
+          if a1.expr_typ = Tint && a1.expr_typ = Tint then
+            (TEbinop (op, a1, a2), Tint, false)
           else error loc "tu opérates sur des non int"
       | Band | Bor ->
           let a1, b1 = expr env e1 in
           let a2, b2 = expr env e2 in
-          if a1.expr_typ = Tbool && a1.expr_typ = Tbool then (TEbinop (op, a1, a2), Tbool, false)
+          if a1.expr_typ = Tbool && a1.expr_typ = Tbool then
+            (TEbinop (op, a1, a2), Tbool, false)
           else error loc "tu logicionnes sur des non bool"
       | Blt | Ble | Bgt | Bge ->
-        let a1, b1 = expr env e1 in
-        let a2, b2 = expr env e2 in
-        if a1.expr_typ = Tint && a1.expr_typ = Tint then (TEbinop (op, a1, a2), Tbool, false)
+          let a1, b1 = expr env e1 in
+          let a2, b2 = expr env e2 in
+          if a1.expr_typ = Tint && a1.expr_typ = Tint then
+            (TEbinop (op, a1, a2), Tbool, false)
           else error loc "tu compares des non int")
-  | PEunop (Uamp, e1) -> (* TODONE *) if (a_t_on_une_l_value e1) then (TEunop(Uamp, fst(expr env e1)), (fst(expr env e1)).expr_typ, false) 
-                                    else error loc "tu ampionnes un non l_value"
+  | PEunop (Uamp, e1) ->
+      (* TODONE *)
+      if a_t_on_une_l_value e1 then
+        (TEunop (Uamp, fst (expr env e1)), (fst (expr env e1)).expr_typ, false)
+      else error loc "tu ampionnes un non l_value"
   | PEunop (Uneg, e1) ->
       (* TODONE *)
       let a1, b1 = expr env e1 in
@@ -142,54 +158,57 @@ and expr_desc env loc = function
   | PEunop (Ustar, e1) ->
       (* TODONE *)
       let a1, b1 = expr env e1 in
-      if not( a1.expr_desc = TEnil ) then (TEunop (Ustar, a1), a1.expr_typ, false)
+      if not (a1.expr_desc = TEnil) then (TEunop (Ustar, a1), a1.expr_typ, false)
       else error loc "tu staronnes un nill"
-
-
-  | PEcall ({ id = "fmt.Print" }, el) -> (* TODO *) (TEprint [], tvoid, false)
-  | PEcall ({ id = "new" }, [ { pexpr_desc = PEident { id } } ]) ->
-      let ty =
-        match id with
-        | "int" -> Tint
-        | "bool" -> Tbool
-        | "string" -> Tstring
-        | _ -> (* TODO *) error loc ("no such type " ^ id)
+  | PEcall ({ id = "fmt.Print" }, el) ->
+      (* TODONE *)
+      fmt_used := true;
+      let rec aux l =
+        match l with
+        | [] -> ([], [])
+        | p :: q ->
+            let a1, b1 = expr env p in
+            let toto, titi = aux q in
+            (a1 :: toto, a1.expr_typ :: titi)
+      in
+      let toto, titi = aux el in
+      let titi = unfold_typList titi in
+      (TEprint toto, tvoid, false)
+  | PEcall ({ id = "new" }, [ { pexpr_desc = PEident { id;loc } } ]) ->
+      let ty = type_type (PTident { id;loc })
       in
       (TEnew ty, Tptr ty, false)
   | PEcall ({ id = "new" }, _) -> error loc "new expects a type"
   | PEcall (id, el) -> (* TODO *) assert false
-
-
-  | PEfor (e, b) -> (* TODONE *) 
+  | PEfor (e, b) ->
+      (* TODONE *)
       let a1, b1 = expr env e in
       let a2, b2 = expr env b in
       if a1.expr_typ = Tbool then (TEfor (a1, a2), tvoid, false)
       else error loc "tu foronnes un non bool"
-
-  | PEif (e1, e2, e3) -> (* TODONE *) 
+  | PEif (e1, e2, e3) ->
+      (* TODONE *)
       let a1, b1 = expr env e1 in
       let a2, b2 = expr env e2 in
       let a3, b3 = expr env e3 in
       if a1.expr_typ = Tbool then (TEif (a1, a2, a3), tvoid, b2 && b3)
       else error loc "tu ifonnes un non bool"
-
   | PEnil ->
-      (*TODONE*)
-      (TEnil, tvoid, false)
-
-      
-  | PEident { id } -> (
-      (* TODO *)
+      (* TODONE *)
+      (TEnil, Tptr Twild, false)
+  | PEident { id; loc } -> (
+      (* TODONE *)
       try
         let v = Env.find id env in
+        v.v_used <- true;
         (TEident v, v.v_typ, false)
-      with Not_found -> error loc ("unbound variable " ^ id))
+      with Not_found -> error loc "Ta variable est introuvable !")
   | PEdot (e, id) -> (* TODO *) assert false
   | PEassign (lvl, el) -> (* TODO *) (TEassign ([], []), tvoid, false)
   | PEreturn el -> (* TODO *) (TEreturn [], tvoid, true)
   | PEblock el -> (* TODO *) (TEblock [], tvoid, false)
   | PEincdec (e, op) -> (* TODO *) assert false
-  | PEvars _ -> (* TODO *) assert false
+  | PEvars _ -> error loc "Unexpected déclaration de var !"
 
 let found_main = ref false
 
@@ -199,13 +218,20 @@ let phase1 = function
   | PDstruct { (*TODONE *) ps_name = { id; loc } } ->
       let dskldhjlsdqfjs = { id; loc } in
       if Hashtbl.mem structure_env dskldhjlsdqfjs.id then
-        error loc "Déjà utilisé ton id mon coco"
+        error loc "Déjà utilisé ton id, petit golem"
       else
-        let titi = { s_name = dskldhjlsdqfjs.id; s_fields = Hashtbl.create 3; s_size = 0 } in
+        let titi =
+          {
+            s_name = dskldhjlsdqfjs.id;
+            s_fields = Hashtbl.create 3;
+            s_size = 0;
+          }
+        in
         Hashtbl.add structure_env dskldhjlsdqfjs.id titi
   | PDfunction _ -> ()
 
-  let rec sizeof = function (* TODONE *) 
+let rec sizeof = function
+  (* TODONE *)
   | Tint | Tbool | Tstring | Tptr _ -> 8
   | Tmany l -> List.fold_left (fun a b -> a + sizeof b) 0 l
   | Tstruct x -> x.s_size
@@ -213,109 +239,150 @@ let phase1 = function
 
 (* 2. declare functions and type fields *)
 let rec type_bien_forme = function
-  |PTident {id; loc}-> id = "bool" || id = "int" || id = "string" || Hashtbl.mem structure_env id
-  |PTptr t -> type_bien_forme t  
+  | PTident { id; loc } ->
+      id = "bool" || id = "int" || id = "string" || Hashtbl.mem structure_env id
+  | PTptr t -> type_bien_forme t
 
 let phase2 = function
-  | PDfunction { pf_name = { id; loc }; pf_params = pl; pf_typ = tyl } -> (* TODONE *) 
-      let kljkksfdf = {id;loc} in
+  | PDfunction { pf_name = { id; loc }; pf_params = pl; pf_typ = tyl } ->
+      (* TODONE *)
+      let kljkksfdf = { id; loc } in
 
-      if  id = "main" && pl = [] && tyl = [] then
-          found_main := true
-      else (if id = "main" then
-        error loc "Ton main est mal défini mon coco")
-      ;
-
-
+      if id = "main" && pl = [] && tyl = [] then found_main := true
+      else if id = "main" then error loc "Ton main est mal défini, petit golem";
 
       if Hashtbl.mem fonction_env kljkksfdf.id then
-        error loc "Déjà utilisé ton id de ta fonction mon coco"
-      else begin
-
+        error loc "Déjà utilisé ton id de ta fonction, petit golem"
+      else
         let new_pl = ref [] in
-        let rec pl_to_new_pl l = (match l with
-          |[]-> ()
-          |p::q -> let x = new_var (fst p).id (fst p).loc (type_type (snd p)) in new_pl:=x::(!new_pl) ; pl_to_new_pl q 
-        )
-        in pl_to_new_pl pl ; 
+        let rec pl_to_new_pl l =
+          match l with
+          | [] -> ()
+          | p :: q ->
+              let x = new_var (fst p).id (fst p).loc (type_type (snd p)) in
+              new_pl := x :: !new_pl;
+              pl_to_new_pl q
+        in
+        pl_to_new_pl pl;
 
         let new_tyl = ref [] in
-        let rec tyl_to_new_tyl l = (match l with
-          |[]-> ()
-          |p::q -> let x =  (type_type p) in new_tyl:=x::(!new_tyl) ; tyl_to_new_tyl q 
-        )
-        in tyl_to_new_tyl tyl ;
+        let rec tyl_to_new_tyl l =
+          match l with
+          | [] -> ()
+          | p :: q ->
+              let x = type_type p in
+              new_tyl := x :: !new_tyl;
+              tyl_to_new_tyl q
+        in
+        tyl_to_new_tyl tyl;
 
-        let titi = { fn_name = kljkksfdf.id; fn_params = !new_pl; fn_typ = !new_tyl} in
+        let titi =
+          { fn_name = kljkksfdf.id; fn_params = !new_pl; fn_typ = !new_tyl }
+        in
         Hashtbl.add fonction_env kljkksfdf.id titi;
-        let nom_variables_de_la_fonction = (Hashtbl.create (List.length pl)) in
+        let nom_variables_de_la_fonction = Hashtbl.create (List.length pl) in
 
-        let rec aux1 l = match l with 
+        let rec aux1 l =
+          match l with
           | [] -> ()
-          | p::q -> 
-            if not(type_bien_forme p) then
-              error loc "Ton type de var est mal formé mon coco"
-            else
-              aux1 q
-        in aux1 tyl;
+          | p :: q ->
+              if not (type_bien_forme p) then
+                error loc "Ton type de var est mal formé, petit golem"
+              else aux1 q
+        in
+        aux1 tyl;
 
-        let rec aux2 l = match l with
+        let rec aux2 l =
+          match l with
           | [] -> ()
-          | p::q -> 
-            if Hashtbl.mem nom_variables_de_la_fonction (fst p).id then
-              error (fst p).loc "Déjà utilisé ton id de ta var mon coco"
-            else
-              (Hashtbl.add nom_variables_de_la_fonction (fst p).id ();
-              if not(type_bien_forme (snd p)) then
-                error (fst p).loc "Ton type de var est mal formé mon coco"
-              else
-                aux2 q)
-        in aux2 pl end
-        
-  | PDstruct { ps_name = { id ; loc }; ps_fields = fl } -> (* TODONE *) 
-    let pqfjmqhf = {id;loc} in
-    try
-      let toto = Hashtbl.find structure_env pqfjmqhf.id in
-      let rec aux l titi = match l with
-        | [] -> titi
-        | p::q -> 
-          if Hashtbl.mem toto.s_fields (fst p).id then
-            error (fst p).loc "Déjà utilisé ton id de ton champ mon coco"
-          else
-            (Hashtbl.add toto.s_fields (fst p).id {f_name = (fst p).id; f_typ = (type_type (snd p)); f_ofs = (titi/8);};
-            if not(type_bien_forme (snd p)) then
-              error (fst p).loc "Ton type de ton champ est mal formé mon coco"
-            else
-              aux q (titi + sizeof (type_type (snd p))))
-      in toto.s_size <- aux fl 0
-    with
-      | Not_found -> error pqfjmqhf.loc ("Ta strucure est introuvable !")
+          | p :: q ->
+              if Hashtbl.mem nom_variables_de_la_fonction (fst p).id then
+                error (fst p).loc "Déjà utilisé ton id de ta var, petit golem"
+              else (
+                Hashtbl.add nom_variables_de_la_fonction (fst p).id ();
+                if not (type_bien_forme (snd p)) then
+                  error (fst p).loc "Ton type de var est mal formé, petit golem"
+                else aux2 q)
+        in
+        aux2 pl
+  | PDstruct { ps_name = { id; loc }; ps_fields = fl } -> (
+      (* TODONE *)
+      let pqfjmqhf = { id; loc } in
+      try
+        let toto = Hashtbl.find structure_env pqfjmqhf.id in
+        let rec aux l titi =
+          match l with
+          | [] -> titi
+          | p :: q ->
+              if Hashtbl.mem toto.s_fields (fst p).id then
+                error (fst p).loc
+                  "Déjà utilisé ton id de ton champ, petit golem"
+              else (
+                Hashtbl.add toto.s_fields (fst p).id
+                  {
+                    f_name = (fst p).id;
+                    f_typ = type_type (snd p);
+                    f_ofs = titi / 8;
+                  };
+                if not (type_bien_forme (snd p)) then
+                  error (fst p).loc
+                    "Ton type de ton champ est mal formé, petit golem"
+                else aux q (titi + sizeof (type_type (snd p))))
+        in
+        toto.s_size <- aux fl 0
+      with Not_found -> error pqfjmqhf.loc "Ta strucure est introuvable !")
 
 (* 3. type check function bodies *)
 let decl = function
-  | PDfunction { pf_name = { id; loc }; pf_body = e; pf_typ = tyl } ->
+  | PDfunction
+      { pf_name = { id; loc }; pf_body = e; pf_typ = tyl; pf_params = pl } ->
       (* TODO check name and type *)
       let toto = ref Env.empty in
-      let titi = ref [] in
-      let f = { fn_name = id; fn_params = !titi; fn_typ = List.map type_type tyl } in
+      let new_pl = ref [] in
+      let rec pl_to_new_pl l =
+        match l with
+        | [] -> ()
+        | p :: q ->
+            let x = new_var (fst p).id (fst p).loc (type_type (snd p)) in
+            new_pl := x :: !new_pl;
+            pl_to_new_pl q
+      in
+      pl_to_new_pl pl;
+
+      let new_tyl = ref [] in
+      let rec tyl_to_new_tyl l =
+        match l with
+        | [] -> ()
+        | p :: q ->
+            let x = type_type p in
+            new_tyl := x :: !new_tyl;
+            tyl_to_new_tyl q
+      in
+      tyl_to_new_tyl tyl;
+
+      let f = { fn_name = id; fn_params = !new_pl; fn_typ = !new_tyl } in
       let e, rt = expr !toto e in
       TDfunction (f, e)
-
-  | PDstruct { ps_name = { id;loc } } ->
+  | PDstruct { ps_name = { id; loc } } -> (
       (* TODONE *)
-      let msqkfhmqo = {id;loc} in
+      let msqkfhmqo = { id; loc } in
       let s = { s_name = id; s_fields = Hashtbl.create 5; s_size = 0 } in
       try
         let toto = Hashtbl.find structure_env msqkfhmqo.id in
         let rec aux x =
-          Hashtbl.iter (fun _ field -> match field.f_typ with
-                                      | Tstruct a -> if a.s_name = id then error dummy_loc ("Ta strucure est récursive !")
-                                                      else aux a.s_fields
-                                      | _ -> (); ) x
-        in aux toto.s_fields;
+          Hashtbl.iter
+            (fun _ field ->
+              match field.f_typ with
+              | Tstruct a ->
+                  if a.s_name = id then
+                    error dummy_loc "Ta strucure est récursive !"
+                  else aux a.s_fields
+              | _ -> ())
+            x
+        in
+        aux toto.s_fields;
         TDstruct s
-      with
-        | Not_found -> error msqkfhmqo.loc ("Ta strucure est introuvable !")
+      with Not_found -> error msqkfhmqo.loc "Ta strucure est introuvable !")
 
 let file ~debug:b (imp, dl) =
   debug := b;
